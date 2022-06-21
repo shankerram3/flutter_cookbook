c5b68d053333107c3786ad6248bd80f5aadacb31
ram.sr <ram.sr@pitsolutions.com>
2022-06-21 19:09:48 +0530

    second commit

---
+++ lib/wrapper.dart
@@ -0,0 +1,32 @@
+import 'package:cookbook/homepage.dart';
+import 'package:cookbook/services/auth_service.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../Models/User_model.dart';
+import '../screens/login_page.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final authService = Provider.of<AuthService>(context);
+    return StreamBuilder<User?>(
+        stream: authService.user,
+        builder: (_,AsyncSnapshot<User?>snapshot){
+          if(snapshot.connectionState == ConnectionState.active ){
+            final User? user = snapshot.data;
+            return user == null ? const LoginPage():const HomePage();
+          }
+          else {
+            return const Scaffold(
+              body: Center(child: CircularProgressIndicator(),
+            ),
+            );
+          }
+    }
+  );
+  }
+}
